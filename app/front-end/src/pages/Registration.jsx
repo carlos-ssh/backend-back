a360bf1ddeba411a9d444798c5a5722e192584f9
CARLOS-SSH <carlos.anezeh@gmail.com>
2022-05-25 13:45:20 -0500

    make it better

---
+++ app/front-end/src/pages/Registration.jsx
@@ -0,0 +1,11 @@
+import SignUp from "../components/forms/SignUp"
+
+const Registration = () => {
+  return (
+    <div className="">
+      <SignUp />
+    </div>
+  )
+}
+
+export default Registration
